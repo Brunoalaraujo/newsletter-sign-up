21e1099180f4fe3c097a08d980b85235786a0837
Bruno Araujo <brunoandradel.a.dev@gmail.com>
2023-07-11 17:02:11 -0300

    Message subscribing

--- src/js/vaditaion.js
+++ src/js/vaditaion.js
@@ -1,8 +1,38 @@
+// Sign up
 function validation() {
-  const emailInput = document.getElementById("email");
-  const emailError = document.getElementById("email-error");
+  var emailInput = document.getElementById("email-input");
+  var spanAlert = document.getElementById("spam-alert");
 
   if (emailInput.value === "") {
-    
+    emailInput.classList.add("form__input-invalid");
+    spanAlert.classList.add("form__invalidText-active");
+    return false;
+  } else if (!validateEmail(emailInput.value))
+  {
+    emailInput.classList.add("form__input-invalid");
+    spanAlert.classList.add("form__invalidText-active");
+    return false;
+  } else {
+    emailInput.classList.remove("form__input-invalid");
+    spanAlert.classList.remove("form__invalidText-active");
+    return true;
   }
+  emailConfirmatio()
+}
+
+function validateEmail(emailInput) {
+  var emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return emailRegex.test(emailInput)
+}
+
+//Confirmation message
+
+function emailConfirmatio(){
+  var email = new URLSearchParams(window.location.search).get('email-input');
+  var emailSpan = document.getElementById("emailSubscribed");
+  emailSpan.textContent = email
+}
+
+function redirectToIndex() {
+  window.location.href = "../../../index.html";
 }
