946d907d685fa26f072638eb52aad02748389889
Bruno Araujo <brunoandradel.a.dev@gmail.com>
2023-07-10 21:51:54 -0300

    Validation function

---
+++ src/js/vaditaion.js
@@ -0,0 +1,8 @@
+function validation() {
+  const emailInput = document.getElementById("email");
+  const emailError = document.getElementById("email-error");
+
+  if (emailInput.value === "") {
+    
+  }
+}
